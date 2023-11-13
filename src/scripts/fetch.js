c3417394c9862a525fa70f27ece1825e85c6b786
u2455785@rts-365.dk <u2455785@rts-365.dk>
2023-11-13 13:53:14 +0100

    fetch

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -1,14 +1,23 @@
 // https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=OYqBSK4vpUhaipVjzAfR4CDSSckHz5Y2
 
-fetch("https://api.nytimes.com/svc/topstories/v2/world.json?api-key=OYqBSK4vpUhaipVjzAfR4CDSSckHz5Y2")
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        data.results.forEach(element => {
-            const LI = document.createElement("li")
-            const UL = document.querySelector(".world__list")
-            LI.innerHTML = `
+const dropDown = document.querySelector(".category__list")
+const list = document.querySelector(".list__items")
+
+dropDown.addEventListener("click", () => {
+    if (list.style.display === "none") {
+        list.style.display = "block"
+    } else {
+        list.style.display = "none"
+    }
+    fetch("https://api.nytimes.com/svc/topstories/v2/world.json?api-key=OYqBSK4vpUhaipVjzAfR4CDSSckHz5Y2")
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            data.results.forEach(element => {
+                const LI = document.createElement("li")
+                const UL = document.querySelector(".list__items")
+                LI.innerHTML = `
             <h2>
                 ${element.title}
             </h2>
@@ -16,7 +25,64 @@ fetch("https://api.nytimes.com/svc/topstories/v2/world.json?api-key=OYqBSK4vpUha
             ${element.abstract}
             </p>
             `
-            UL.append(LI)
+                UL.append(LI)
+            })
         })
-    })
-    
+
+})
+
+dropDown.addEventListener("click", () => {
+    if (list.style.display === "none") {
+        list.style.display = "block"
+    } else {
+        list.style.display = "none"
+    }
+    fetch("https://api.nytimes.com/svc/topstories/v2/world.json?api-key=OYqBSK4vpUhaipVjzAfR4CDSSckHz5Y2")
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            data.results.forEach(element => {
+                const LI = document.createElement("li")
+                const UL = document.querySelector(".list__items")
+                LI.innerHTML = `
+            <h2>
+                ${element.title}
+            </h2>
+            <p>
+            ${element.abstract}
+            </p>
+            `
+                UL.append(LI)
+            })
+        })
+
+})
+
+dropDown.addEventListener("click", () => {
+    if (list.style.display === "none") {
+        list.style.display = "block"
+    } else {
+        list.style.display = "none"
+    }
+    fetch("https://api.nytimes.com/svc/topstories/v2/sport.json?api-key=OYqBSK4vpUhaipVjzAfR4CDSSckHz5Y2")
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            data.results.forEach(element => {
+                const LI = document.createElement("li")
+                const UL = document.querySelector(".list__items")
+                LI.innerHTML = `
+            <h2>
+                ${element.title}
+            </h2>
+            <p>
+            ${element.abstract}
+            </p>
+            `
+                UL.append(LI)
+            })
+        })
+
+})
