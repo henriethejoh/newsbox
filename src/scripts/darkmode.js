9c542ea4be6ac0ea83c0f642c494cf205213ad80
u2455785@rts-365.dk <u2455785@rts-365.dk>
2023-11-21 09:34:39 +0100

    save darkmode in localstorage

--- src/scripts/darkmode.js
+++ src/scripts/darkmode.js
@@ -1,12 +1,23 @@
-export default//guard caulse
- (function(){
-    if (!window.location.pathname.includes("settings.html"))return
+export default
+    (function () {
+        if (!window.location.pathname.includes("settings.html")) return
 
-    const CTA_BUTTON = document.querySelector(".activate_darkmode--btn")
-    
-    CTA_BUTTON.addEventListener("click", clickHandler)
+        const CTA_BUTTON = document.querySelector(".activate_darkmode--btn")
 
-    function clickHandler(){
-        document.body.classList.toggle("darkmode")
-    }
-})()
+        CTA_BUTTON.addEventListener("click", clickHandler)
+
+        function clickHandler() {
+            const CLASS_LIST = document.body.classList
+            CLASS_LIST.toggle("darkmode")
+            localStorage.setItem("theme",
+                CLASS_LIST.contains("darkmode")
+                    ? "darkmode"
+                    : "")
+        }
+
+        if (!localStorage.getItem("theme")) {
+            localStorage.setItem("theme", "")
+        }
+
+        document.body.classList.add(localStorage.getItem("theme"))
+    })()
